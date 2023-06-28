a230ebaf4298ab0a86d6fff43f351bca322009fc
Rafael Modesto <rafaelmodesto09@gmail.com>
2023-06-27 21:08:32 -0300

    Adicionado validacoes e regras de negocio nas entidades para deixarem de ser entidades anemicas, criado classe abstrata entity para ser base para as demais entidades. Adicionado intefaces e testes unitarios.

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetProductCategoryAsync(int? id);
+
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+        
+    }
+}
